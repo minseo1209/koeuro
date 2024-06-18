2e4a36450e5a39fb4d63feaa16d449b156041d28
김민서 <ekwjd8873@naver.com>
2024-06-18 16:59:39 +0900

    채용 게시판 DATA 수정

--- src/js/Contact/ContactDetail.js
+++ src/js/Contact/ContactDetail.js
@@ -1,53 +1,29 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-function ContactDetail() {
-  const { id } = useParams();
+const ContactDetail = () => {
   const location = useLocation();
-  const contact = location.state?.contact;
-  if (!contact || !contact.content) {
-    return <div>공지사항을 찾을 수 없습니다.</div>;
-  }
+  const { contact } = location.state;
 
   return (
     <div className="container mt-5">
-      <div className="main"></div>
-      <h2 className="AboutBrandStory" style={{ marginBottom: '1em' }}>
-        {contact.title}
-      </h2>
-      <div>
-        {contact.content.map((item, index) => {
-          if (item.type === 'text') {
-            return <p key={index}>{item.value}</p>;
-          } else if (item.type === 'image') {
-            return (
-              <div key={index}>
-                <img
-                  src={item.value}
-                  alt={item.alt}
-                  style={{ width: '50%', margin: '20px 0' }}
-                />
-              </div>
-            );
-          } else if (item.type === 'map') {
-            return (
-              <div
-                key={index}
-                style={{
-                  width: '100%',
-                  height: '400px',
-                  margin: '20px 0',
-                  overflow: 'hidden',
-                }}
-                dangerouslySetInnerHTML={{ __html: item.value }}
-              />
-            );
-          }
-          return null;
-        })}
-      </div>
+      <h2>{contact.title}</h2>
+      <table>
+        <tbody>
+          {contact.content[0].value.map((item, index) => (
+            <tr key={index}>
+              <th>{item.label}</th>
+              <td>
+                {item.text.split('\n').map((line, lineIndex) => (
+                  <div key={lineIndex}>{line}</div>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default ContactDetail;
