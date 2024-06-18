606a23714be5e5b30e5d5e58d478e3b9520ec655
dbdp183 <dbdp183@naver.com>
2024-06-18 16:59:19 +0900

    상단 header bar 수정

--- src/js/NavigationComponent.js
+++ src/js/NavigationComponent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 // import '../css/main.css';
-import styles from "../css/main.module.css";
+import styles from '../css/main.module.css';
 
 //Header
 function NavigationComponent() {
@@ -14,45 +14,21 @@ function NavigationComponent() {
 
   return (
     <header className="fixed-top bg-white px-4 py-4 shadow-sm">
-      <div
-        className={`container ${styles.headerBar}`}
-        id="navbar"
-      >
-        <Link to="/" className="text-decoration-none text-dark fs-4">
-          <img src="../assets/images/logo/logo_o.png" className="logo" />
+      <div className={`container ${styles.headerBar}`}>
+        <Link to="/" className="fs-4">
+          <img src="../assets/images/logo/logo_o.png" className={styles.logo} />
         </Link>
         <nav className="d-none d-md-flex gap-4">
-          <Link to="/" className="text-decoration-none text-dark">
-            홈
-          </Link>
-          <Link to="/about" className="text-decoration-none text-dark">
-            회사소개
-          </Link>
-          <a
-            href="https://koeuro.shop/"
-            className="text-decoration-none text-dark"
-            target="_blank"
-            rel="noopener noreferrer"
-            id="koeuromall"
-          >
+          <Link to="/">홈</Link>
+          <Link to="/about">회사소개</Link>
+          <a href="https://koeuro.shop/" target="_blank" rel="noopener noreferrer" className={styles.koeuromall}>
             코유로 몰
           </a>
-
-          <Link to="/Notices" className="text-decoration-none text-dark">
-            공지사항
-          </Link>
-          <Link to="/contact" className="text-decoration-none text-dark">
-            채용
-          </Link>
-          <Link to="/madang" className="text-decoration-none text-dark">
-            비지니스 문의
-          </Link>
+          <Link to="/Notices">공지사항</Link>
+          <Link to="/contact">채용</Link>
+          <Link to="/madang">비지니스 문의</Link>
         </nav>
-        <Button
-          variant="outline-secondary"
-          className="d-md-none"
-          onClick={toggleMenu}
-        >
+        <Button variant="outline-secondary" className="d-md-none" onClick={toggleMenu}>
           Toggle navigation
         </Button>
       </div>
