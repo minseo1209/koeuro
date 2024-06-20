3e21f7869171b38415a743223f71de4722c8d0b2
dbdp183 <dbdp183@naver.com>
2024-06-20 13:20:36 +0900

    Developing Component 에서 Brand로 링크 이미지 이동. css 수정.

--- src/js/Main/DevelopingComponent.js
+++ src/js/Main/DevelopingComponent.js
@@ -1,46 +1,19 @@
 import { useEffect, useState } from 'react';
 
 import styles from '../../css/main.module.css';
-import aniStyles from '../../css/animation.module.css';
 
 // 메인용 코유로 안내
 
 function DevelopingComponent() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const textWrap = document.querySelector('.developing_textwrap');
-      const positionFromTop = textWrap.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      if (positionFromTop - windowHeight <= 0) {
-        setIsVisible(true);
-        window.removeEventListener('scroll', handleScroll);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <section className={'py-5 developing_textwrap'}>
-      <div className={`developing_text`}>
+    <section className={'py-5'}>
+      <div>
         <h2 className={styles.koeuroTitle}>코유로 &#40;KOEURO&#41;</h2>
-
-        <h4 className={styles.koeurosubtitle}>
+        <h4 className={`${styles.koeurosubtitle}`}>
           한국&#40;Koera&#41;과 유럽&#40;Europe&#41;간의 링크를 상징하는 이름입니다.
         </h4>
-        <img
-          src="../assets/images/logo/KoeuroLink.png"
-          className={`${styles.koeuroLink} ${aniStyles.rotate45} ${isVisible ? aniStyles.appearFromBottom : ''}`}
-        ></img>
         <div className={styles.koeuroExplanation}>
-          <p style={{ marginTop: '-75px' }}>
+          <p>
             우리는 현지에 회사를 운영하고 있어 유럽 제품에 빠른 접근성과 인프라를 가지고 있습니다.
             <br />
             트렌드에 앞서가는 소비자에게 유럽 문화에 대한 정보 및 소개되지 않았던 하이퀄리티 제품을 제공합니다.
