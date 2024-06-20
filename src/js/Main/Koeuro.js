1e573fd18ca12c402f017925b9d3334fe43424fc
dbdp183 <dbdp183@naver.com>
2024-06-20 10:51:25 +0900

    Koeuro css 수정

--- src/js/Main/Koeuro.js
+++ src/js/Main/Koeuro.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import '../../css/animation.module.css';
+import aniStyles from '../../css/animation.module.css';
+import styles from '../../css/main.module.css';
 
 // 메인용 코유로 소개글
 
@@ -26,19 +27,19 @@ function Koeuro() {
   }, []);
 
   return (
-    <div className={`koeuro ${isVisible ? 'appear_from_bottom' : ''}`}>
-      <section className="banner-layout1 pb-0 bg-parallax vision-layout">
-        <div className="thinking">
-          <img src="/assets/images/backgrounds/8.png" alt="img" className="thinkingImg" />
+    <div className={`koeuro ${isVisible ? aniStyles.appearFromBottom : ''}`}>
+      <section className={`${styles.bannerLayoutFirst} pb-0 bg-parallax vision-layout`}>
+        <div className={styles.thinking}>
+          <img src="/assets/images/backgrounds/8.png" alt="img" className={styles.thinkingImg} />
         </div>
         <div className="container-fluid col-padding-0 pb-40">
           <div className="row">
             <div className="col-md-6"></div>
             <div className="col-md-6 vision-text-box-layout flex align-items-center justify-content-end">
-              <div className="vision-text-box" data-aos="fade-left">
-                <div className="white-box">
-                  <h2 className="vision-text-title">항상 고객에게 어떤 이점이 있는지에 초점을 두고 있습니다.</h2>
-                  <h3 className="vision-text-content" id="subtext">
+              <div className={styles.visionTextBox} data-aos="fade-left">
+                <div className={styles.whiteBox}>
+                  <h2 className={styles.visionTextTitle}>항상 고객에게 어떤 이점이 있는지에 초점을 두고 있습니다.</h2>
+                  <h3 className={styles.visionTextContent} id="subtext">
                     해당 브랜드 본사 또는 라이선스를 보유한 업체가 직접 상품을 판매하고 있습니다.
                     <br />
                     상품에 대한 신뢰도나 사후 관리 등의 측면에서 믿고 구매할 수 있습니다.
