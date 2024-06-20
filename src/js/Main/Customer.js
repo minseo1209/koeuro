4be4a51c3618ee1801040e88ca863229eaeeaf42
김민서 <ekwjd8873@naver.com>
2024-06-20 13:44:50 +0900

    Customer 안쓰는 기능 삭제

--- src/js/Main/Customer.js
+++ src/js/Main/Customer.js
@@ -29,20 +29,24 @@ function Customer() {
       name: '마당',
       link1: '마당문의',
       href1: '#',
-      description: '소상공인을 위한 극 최적화 맞춤형 마케팅 서비스 마당에서 시작하세요.',
+      description:
+        '소상공인을 위한 극 최적화 맞춤형 마케팅 서비스 마당에서 시작하세요.',
     },
   ];
-
-  // 이미지 변경을 위한 상태 변수
-  const [hoveredBrand, setHoveredBrand] = useState(null);
 
   return (
     <section>
       <div className={brandStyles.partnerBrandContainer}>
         <div className={brandStyles.partnerBrandContainer}>
           <div className={brandStyles.partnerBrandTitles}>
-            <div className={brandStyles.partnerBrandTitle}>고객 만족 실현을</div>
-            <img src="../assets/images/logo/logo_o.png" className={brandStyles.titleImg} alt="유럽의 명품" />
+            <div className={brandStyles.partnerBrandTitle}>
+              고객 만족 실현을
+            </div>
+            <img
+              src="../assets/images/logo/logo_o.png"
+              className={brandStyles.titleImg}
+              alt="유럽의 명품"
+            />
             <div className={brandStyles.partnerBrandTitle}>에서 만나보세요</div>
           </div>
           <div className={brandStyles.brandsContainer}></div>
@@ -53,10 +57,17 @@ function Customer() {
               <div className={brandStyles.bCard}>
                 <div className={brandStyles.brandCardContent}>
                   <h className={brandStyles.brandName}>{brand.name}</h>
-                  <p className={brandStyles.brandDescription} style={{ fontSize: '14px' }}>
+                  <p
+                    className={brandStyles.brandDescription}
+                    style={{ fontSize: '14px' }}
+                  >
                     {brand.description}
                   </p>
-                  <ul key={index} className={styles.ulList} style={{ margin: '0', padding: '0', fontSize: '11px' }}>
+                  <ul
+                    key={index}
+                    className={styles.ulList}
+                    style={{ margin: '0', padding: '0', fontSize: '11px' }}
+                  >
                     <li className={styles.liList}>
                       <a href={brand.href1} target="_blank">
                         {brand.link1}
