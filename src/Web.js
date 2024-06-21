d279af5c19e162c780309d3720d04f35abff7268
김민서 <ekwjd8873@naver.com>
2024-06-21 13:58:34 +0900

    Web 페이지 이동시 스크롤 위치 상단으로 이동 기능 추가

--- src/Web.js
+++ src/Web.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useLocation } from 'react-router-dom';
 import NavigationComponent from './js/NavigationComponent';
 import { MemoizedMain as Main } from './js/Main/Main';
 import About from './js/About/About';
@@ -14,6 +15,7 @@ import NoticeDetail from './js/Notices/NoticeDetail';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App" style={{ overflowX: 'hidden' }}>
         <NavigationComponent />
         <main>
@@ -30,6 +32,15 @@ function App() {
       </div>
     </Router>
   );
+}
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
 
 export default App;
