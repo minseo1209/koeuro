8907fe63ffc65047c832bca8613aeb5838ef7037
김민서 <ekwjd8873@naver.com>
2024-06-24 15:53:37 +0900

    Nav바 모바일 버튼 추가

--- src/js/NavigationComponent.js
+++ src/js/NavigationComponent.js
@@ -21,15 +21,24 @@ function NavigationComponent() {
         <nav className="d-none d-md-flex gap-4">
           <Link to="/">홈</Link>
           <Link to="/about">회사소개</Link>
-          <a href="https://koeuro.shop/" target="_blank" rel="noopener noreferrer" className={styles.koeuromall}>
+          <a
+            href="https://koeuro.shop/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.koeuromall}
+          >
             코유로 몰
           </a>
           <Link to="/Notices">공지사항</Link>
           <Link to="/contact">채용</Link>
           <Link to="/madang">비지니스 문의</Link>
         </nav>
-        <Button variant="outline-secondary" className="d-md-none" onClick={toggleMenu}>
-          Toggle navigation
+        <Button
+          variant="outline-secondary"
+          className="d-md-none"
+          onClick={toggleMenu}
+        >
+          三
         </Button>
       </div>
     </header>
