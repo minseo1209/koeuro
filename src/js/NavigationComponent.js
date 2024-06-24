8fb06a3574dd065b42f5ce91bc2a3dc50b802fb3
김민서 <ekwjd8873@naver.com>
2024-06-24 16:33:38 +0900

    Nav바 모바일 메뉴 개선

--- src/js/NavigationComponent.js
+++ src/js/NavigationComponent.js
@@ -16,7 +16,11 @@ function NavigationComponent() {
     <header className="fixed-top bg-white px-4 py-4 shadow-sm">
       <div className={`container ${styles.headerBar}`}>
         <Link to="/" className="fs-4">
-          <img src="../assets/images/logo/logo_o.png" className={styles.logo} />
+          <img
+            src="../assets/images/logo/logo_o.png"
+            className={styles.logo}
+            alt="Logo"
+          />
         </Link>
         <nav className="d-none d-md-flex gap-4">
           <Link to="/">홈</Link>
@@ -41,6 +45,47 @@ function NavigationComponent() {
           三
         </Button>
       </div>
+      {menuOpen && (
+        <div className={styles.mobileMenu}>
+          <ul>
+            <li>
+              <Link to="/" onClick={toggleMenu}>
+                홈
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={toggleMenu}>
+                회사소개
+              </Link>
+            </li>
+            <li>
+              <a
+                href="https://koeuro.shop/"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={toggleMenu}
+              >
+                코유로 몰
+              </a>
+            </li>
+            <li>
+              <Link to="/Notices" onClick={toggleMenu}>
+                공지사항
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={toggleMenu}>
+                채용
+              </Link>
+            </li>
+            <li>
+              <Link to="/madang" onClick={toggleMenu}>
+                비지니스 문의
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
